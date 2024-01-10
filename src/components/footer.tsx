8bd8b77dd7160727fc8f5644a46a9009f1bd4801
TheGiantNinja <yasserfedsi34@gmail.com>
2024-01-10 21:02:29 +0100

    Small changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,6 @@ import "../styles/footer.css";
 export default function Footer() {
   const year = new Date().getFullYear();
   const linkTo = "https://yasser.is-a.dev";
-  const bacResources = "https://bacwithabou-bakr.netlify.app/";
-  const borhanInstagram = "https://instagram.com/bober.byte/";
-  const younesInstagram = "https://instagram.com/younes_aithamou";
 
   return (
     <footer className="footer">
@@ -24,27 +21,6 @@ export default function Footer() {
             Yasser Fedsi
           </a>
           . All rights reserved.
-          <p className="small_text ">
-            © Thanks to{" "}
-            <a href={bacResources} target="_blank">
-              AbuBakr BenZerka
-            </a>{" "}
-            for pictures.
-          </p>
-          <p className="small_text ">
-            © Thanks to{" "}
-            <a href={borhanInstagram} target="_blank">
-              BorhanEddin Bouhadjar
-            </a>{" "}
-            for testing.
-          </p>
-          <p className="small_text ">
-            © Thanks to{" "}
-            <a href={younesInstagram} target="_blank">
-              Younes Aithamou
-            </a>{" "}
-            for drivers.
-          </p>
         </p>
       </div>
     </footer>
